25ef6695ffa8b0155c076bbce71acc30c4f58bbf
Alex Santos de Oliveira <119976670+alex-oliveira1@users.noreply.github.com>
2023-01-16 20:18:08 -0300

    parte 2 aulas 6
    final aula 6

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ var saida = document.getElementById("saida");
 var imprimir= "";
 var semana = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado"];
 // alert(semana.length);
-
+alert(semana.push("Oitava","Sabado 2", "Domingo 2"));// adiciona ao final
+alert(semana.pop());// tira o ultimo
+semana.shift();//tira o primeiro
+alert(semana.indexOf("Quinta"));
 for (var i=0;i<semana.length;i++){
        imprimir += semana[i] + "<br>";
 }
